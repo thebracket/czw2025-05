5617a05bbeeea8f4e65258e465de1e53eecdc9e1
Herbert Wolverson <herberticus@gmail.com>
2025-05-06 14:43:49 -0500

    Tokio - async

--- live_day2/first_thread/src/main.rs
+++ live_day2/first_thread/src/main.rs
@@ -1,9 +1,36 @@
-use rayon::prelude::*;
+async fn hello() {
+    println!("Hello, world!");
+}
 
-fn main() {
-    let numbers: Vec<u64> = (0 .. 1_000_000).collect();
-    let sum = numbers
-        .par_iter()
-        .sum::<u64>();
-    println!("{sum}");
+async fn double(n: i32) -> i32 {
+    n * 2
+}
+
+use std::time::Duration;
+use tokio::task::spawn_blocking;
+
+async fn hello_delay(task: u64, time: u64) {
+    println!("Task {task} has started");
+    tokio::time::sleep(Duration::from_millis(time)).await;
+    let result = spawn_blocking(move || {
+        std::thread::sleep(Duration::from_millis(time));
+    }).await;
+    println!("Task {task} is done.");
+}
+
+#[tokio::main]
+async fn main() {
+    let result = tokio::join!(double(4), double(2));
+    hello().await;
+    hello_delay(10, 5).await;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    
+    #[tokio::test]
+    async fn test_double() {
+        
+    }
 }
