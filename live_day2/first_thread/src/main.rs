9b9c623da9a02de6b2604baf21d1e9923e124019
Herbert Wolverson <herberticus@gmail.com>
2025-05-06 14:03:31 -0500

    Day 2 Hour 2 Wrap, Rayon

--- live_day2/first_thread/src/main.rs
+++ live_day2/first_thread/src/main.rs
@@ -1,29 +1,9 @@
-use std::sync::mpsc;
-
-enum Command {
-    SayHello, Quit
-}
+use rayon::prelude::*;
 
 fn main() {
-    let (tx, rx) = mpsc::channel::<Command>();
-
-    let handle = std::thread::spawn(move || {
-        while let Ok(command) = rx.recv() {
-            match command {
-                Command::SayHello => println!("Hello"),
-                Command::Quit => {
-                    println!("Quitting now");
-                    break;
-                }
-            }
-        }
-    });
-
-    let my_tx = tx.clone();
-    for _ in 0 .. 10 {
-        tx.send(Command::SayHello).unwrap();
-    }
-    println!("Sending quit");
-    tx.send(Command::Quit).unwrap();
-    handle.join().unwrap();
+    let numbers: Vec<u64> = (0 .. 1_000_000).collect();
+    let sum = numbers
+        .par_iter()
+        .sum::<u64>();
+    println!("{sum}");
 }
